56319ffb1c2beb9ba0f9ba7dbbc5b10de2cccd87
Martin Boudreau <mboudrea\cstj.qc.ca>
2024-07-22 10:50:15 -0400

    Ménage

---
+++ RADME.md
@@ -0,0 +1,7 @@
+# Github Proxy
+
+My personal proxy for GitHub.
+
+Allows me to access GitHub include the `raw.githubusercontent.com` in my projects so I can access files from tags and branches wothout having to use GitHub Pages.
+
+![GitHub Proxy](logo.svg)
--- index.php
+++ index.php
@@ -7,10 +7,5 @@ include_once 'proxy.php';
 // 	include 'home.html';
 // 	die;
 // }
-// if (realpath(__DIR__ . $_SERVER['PHP_SELF']) !== __FILE__) {
-// 	Mime::header($_SERVER['SCRIPT_FILENAME']);
-// 	include $_SERVER['SCRIPT_FILENAME'];
-// 	die;
-// }
 Proxy::$allowed_users = ['bobanum', 'else-angels', 'js-cstj', 'tim-cstj', 'vue-cstj', 'web1cstj', 'web2cstj', 'web3cstj', 'web4cstj',];
 Proxy::call();
